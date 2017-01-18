95013e5faaea5790d8e294ff2b6aa8c4307f19f5
emilMircea <emil.mircea30@gmail.com>
2017-01-18 16:02:05 +0100

    added static graphs

--- js/main.js
+++ js/main.js
@@ -2,16 +2,6 @@ const circles = document.querySelectorAll('.change-colors > button');
 const links = Array.from(document.querySelectorAll('nav > .link > button'));
 const svgs = Array.from(document.querySelectorAll('.icon'));
 const fontSize = document.getElementById('fontSizeInput')
-// endpoint for ajax data coming from API
-const endpoint = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-const currencies = [];
-const dataObj = fetch(endpoint)
-  .then(blob => blob.json())
-    .then(data => currencies.push(
-      data.bpi.EUR.description, data.bpi.EUR.rate,
-      data.bpi.USD.description,data.bpi.USD.rate,
-      data.bpi.GBP.description, data.bpi.GBP.rate
-    ));
 
 function handleClick() {
   links.map(link => link.style.setProperty(`${this.name}`, this.value));
@@ -20,10 +10,7 @@ function handleClick() {
 circles.forEach(circle => circle.addEventListener('click', handleClick));
 // console.log(currencies);
 const insideCard = document.getElementById('api-data');
-// window.onload function needed because u create elem before loading page
-window.onload = function() {
-  insideCard.innerHTML = currencies;
-}
+
 
 // CHANGE FONT SIZE FOR TITLE
 const inputs = document.querySelectorAll('.controls input');
